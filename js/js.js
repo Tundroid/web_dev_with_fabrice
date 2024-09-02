b16dd6f5eaa57eafcacd9e4448eabbe44fe7ff80
fabrice njong <fabricenjong1@gmail.com>
2024-09-02 20:05:39 +0100

    js

--- js/js.js
+++ js/js.js
@@ -6,3 +6,18 @@ function changeBGColor(btn){
     console.log(`RGB(${green}, ${blue}, ${red})`);
     document.getElementsByTagName("body")[0].style.backgroundColor = `RGB(${green}, ${blue}, ${red})`;
 }
+document.getElementById("myID0").style.color = "#4211aa";
+document.getElementsByClassName("heading")[0].style.textDecoration = "underline";
+function addFont(btn){
+    currentSize = parseInt(getComputedStyle(document.getElementById("head0")).fontSize);
+    newSize = currentSize + 1;
+    document.getElementById("head0").style.fontSize = newSize + 'px';
+    console.log("Font Size added");
+}
+function reduceFont(btn){
+    currentSize = parseInt(getComputedStyle(document.getElementById("head0")).fontSize);
+    newSize = currentSize - 1;
+    document.getElementById("head0").style.fontSize = newSize + 'px';
+    console.log("Font Size reduced");
+}
+    
